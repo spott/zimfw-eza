fe12d87dc28fdc977a71804105323ad5a88dd6e5
Andrew Spott <andrew.spott@gmail.com>
2023-09-07 09:01:48 -0600

    Update init.zsh to use eza instead

--- init.zsh
+++ init.zsh
@@ -1,11 +1,11 @@
-# Ensure exa is available
-if (( ! ${+commands[exa]} )); then
+# Ensure eza is available
+if (( ! ${+commands[eza]} )); then
   return 1
 fi
 
 export EXA_COLORS='da=1;34:gm=1;34'
 
-alias ls='exa --group-directories-first'
+alias ls='eza --group-directories-first'
 alias ll='ls -l --git'        # Long format, git status
 alias l='ll -a'               # Long format, all files
 alias lr='ll -T'              # Long format, recursive as a tree
